554a749007b7406bcd7e63b22c7467a38dab36e0
weiss25r <raffaeleterracino001@gmail.com>
2022-04-25 19:55:03 +0200

    Added "selection_sort.c"

---
+++ Sorting/Selection sort/selection_sort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//selection sorts an array of n elements
+void swap(int *a, int *b) {
+    int *tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void selectionSort(int *array, int n) {
+    for(int i = 0; i < n; i++) {
+        int min = i;
+
+        for(int j = i+1; j < n; j++) {
+            if(array[j] < array[min])
+                min = j;
+        }
+
+        if(min != i)
+            swap(&array[i], &array[min]);
+    }
+}
+
+void printArray(int *array, int n) {
+    for (size_t i = 0; i < n; i++)
+        printf("%d\t", array[i]);
+    
+    puts("");
+}
+
+int main(void) {
+    int test[] = {5, 4, 3, 2, 1};
+    
+    puts("Unsorted array: ");
+    printArray(test, 5);
+
+    selectionSort(test, 5);
+
+    puts("Sorted array");
+    printArray(test, 5);
+}
